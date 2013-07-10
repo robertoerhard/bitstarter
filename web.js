1de5e1580315b0c87ad30c7f6c158e007f137f14
Roberto Erhard <roberto.erhard@gmail.com>
2013-07-10 04:45:05 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var indexfile = new Buffer(fs.readFileSync('index.html'));
   response.send(indexfile.toString());
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+});
+  var port = process.env.PORT || 5000;
+  app.listen(port, function() {
   console.log("Listening on " + port);
 });
